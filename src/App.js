133d5a63ddfd5fd5e121a1677d686f2894256400
jlanp3 <jlanp3@gmail.com>
2024-02-18 22:13:44 -0300

    using libs

--- src/App.js
+++ src/App.js
@@ -1,55 +1,19 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Editor from "./Components/Editor";
 import Preview from "./Components/Preview";
 
 function App() {
-  const [text, setText] = useState([]);
-  const [arr, setArr] = useState([]);
+  const [text, setText] = useState("");
 
   const handleChange = (e) => {
-    let res = e.target.value;
-    res = res.split(/\n/);
-    setText(res);
+    setText(e.target.value);
   };
 
-  const search = useCallback((text) => {
-    let res = text.map((char) => {
-      // symbols(char);
-      if (char.match(/^[#][#]*/)) {
-        return header(char);
-      } else {
-        return parag(char);
-      }
-    });
-    setArr(res);
-  }, []);
-
-  useEffect(() => {
-    search(text);
-  }, [search, text]);
-
-  const header = (data) => {
-    let num = data.match(/^[#][#]*/)[0].length;
-    data = data.replace(/^[#][#]*/, "").trim();
-    const CustomTag = `h${num}`;
-    let custom = <CustomTag>{data}</CustomTag>;
-    return custom;
-  };
-
-  const parag = (data) => {
-    if (data.match(/^\s|^\w|^\W/)) {
-      return <p>{data}</p>;
-    }
-  };
-
-  const symbols = (data) => {};
-
-  console.log(text, arr);
   return (
     <div className="App">
       <h1>markdown</h1>
-      <Editor handleChange={handleChange} />
+      <Editor handleChange={handleChange} value={text} />
       <Preview />
     </div>
   );
